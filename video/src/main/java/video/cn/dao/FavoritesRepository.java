8087277f06b85b322abfe9b496bba0053e6b8f30
violin256 <2334366359@qq.com>
2020-05-03 18:29:13 +0800

    publish gh-pages

--- video/src/main/java/video/cn/dao/FavoritesRepository.java
+++
@@ -1,9 +0,0 @@
-package video.cn.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import video.cn.pojo.Favorites;
-import video.cn.pojo.Feedback;
-@Repository
-public interface FavoritesRepository extends JpaRepository<Favorites,Integer> {
-}
--- video/src/main/java/video/cn/service/FeedbackService.java
+++
@@ -1,8 +0,0 @@
-package video.cn.service;
-
-import video.cn.pojo.Feedback;
-
-public interface FeedbackService {
-    //添加反馈信息
-    public void addFeedbackService(Feedback feedback);
-}
